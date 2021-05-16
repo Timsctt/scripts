63be56222cde3e6c45c370aa0628bd2d3e918b4f
Timsht <timothee.schmitt@hotmail.fr>
2021-05-16 23:42:42 +0200

    make own changement

--- LINUX/Arch/base.sh
+++ LINUX/Arch/base.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# inspired by : EF - Linux Made Simple
+# youtube channel : https://www.youtube.com/c/EFLinuxMadeSimple/featured
+# https://gitlab.com/eflinux/arch-basic
+
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
+sed -i 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=fr_FR.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
+echo "KEYMAP=fr" >> /etc/vconsole.conf
+
+read -p "Choose a hostname : " HOSTNAME
+echo $HOSTNAME >> /etc/hostname
+
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset firewalld flatpak sof-firmware nss-mdns acpid
-
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset firewalld flatpak sof-firmware nss-mdns acpid nodejs htop vim
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -32,9 +36,10 @@ systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
 
-useradd -m timarch
-echo timarch:password | chpasswd
-usermod -aG libvirt timarch
+read -p "Choose a username : " USER_NAME
+useradd -m $USER_NAME
+echo $USER_NAME:password | chpasswd
+usermod -aG libvirt $USER_NAME
 
 echo "timarch ALL=(ALL) ALL" >> /etc/sudoers.d/timarch
 
